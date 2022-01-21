68a705afc719f717be174951dd87a517d6edda8d
robotlions <>
2022-01-21 09:56:03 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import react from 'react';
 
 function App() {
+
+const [generatedGenre, setGeneratedGenre] = useState(null);
+const [generatedTheme, setGeneratedTheme] = useState(null);
+const [generatedCharacter, setGeneratedCharacter] = useState(null);
+
+
+function doGenre() {
+    setGeneratedGenre("New Genre")
+  };
+
+function doTheme() {
+    setGeneratedTheme("New Theme")
+  };
+
+function doCharacter() {
+    setGeneratedCharacter("New Character")
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Random Story-Generating Radomizer for Stories</h2>
+      <button onClick={()=>doGenre()}>Genre</button>
+      <button onClick={()=>doTheme()}>Theme</button>
+      <button onClick={()=>doCharacter()}>Character</button>
+      <br/>
+      Genre: {generatedGenre}<br/>
+      Theme: {generatedTheme}<br/>
+      Character: {generatedCharacter}<br/>
     </div>
   );
 }
