d7464d6fc8a4638e5ed489361df58bc50ce9d84c
robotlions <>
2022-01-21 13:48:40 -0500

    adds styling and data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
-import react from 'react';
 import {genreList} from './genres';
 import {themeList} from './themes';
 import {characterList} from './characters';
+import {Button, Container} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
@@ -33,16 +34,26 @@ function doRandom(min, max) {
   }
 
   return (
-    <div className="App">
+      <div className="container">
+      <div className="row header">
       <h2>Random Story-Generating Randomizer for Stories</h2>
-      <button onClick={()=>doGenre()}>Genre</button>
-      <button onClick={()=>doTheme()}>Theme</button>
-      <button onClick={()=>doCharacter()}>Character</button>
+      </div>
+      <div className="buttonRow">
+      <Button onClick={()=>doGenre()}>Genre</Button>
+      <Button onClick={()=>doTheme()}>Theme</Button>
+      <Button onClick={()=>doCharacter()}>Character</Button>
+      </div>
       <br/>
       <p className="headingName">Genre: <span className="generatedString">{generatedGenre}</span></p>
       <p className="headingName">Theme: <span className="generatedString">{generatedTheme}</span></p>
       <p className="headingName">Character: <span className="generatedString">{generatedCharacter}</span></p>
-
+<br/>
+<br/>
+<div className="creditsRow">
+<p>List of genres stolen from <a href="https://www.servicescape.com/blog/144-genres-and-subgenres-for-fiction-writing">Tanya Thompson.</a></p>
+<p>List of themes stolen from <a href="https://literarydevices.net/a-huge-list-of-common-themes/">literarydevices.net</a></p>
+<p>List of characters stolen from <a href="https://tvtropes.org/pmwiki/pmwiki.php/Main/ArchetypalCharacter">TV Tropes.</a></p>
+   </div>
     </div>
   );
 }
